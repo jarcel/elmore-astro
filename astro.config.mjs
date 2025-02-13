1463c475ffc0f193d4ab16edefb1babd9f6b8324
jarcel <jeff@jacapps.com>
2025-02-13 12:33:30 -0500

    Changed site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.APP_URL,
+  site: process.env.APP_URL,
   integrations: [sitemap()],
 })
